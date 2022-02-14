d95a3213f64bc2cc0b1da1b4e83c45726892e708
Vishal Sharma <vishalsharmaoct3@gmail.com>
2022-02-14 13:02:40 -0600

    Added Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,9 @@
 public class Diff{
-    
+    public static void main (String [] args){
+        int a = 5;
+        int b = 6;
+
+        System.out.println("Values of Difference : "+ (b-a));
+
+    }
 }
